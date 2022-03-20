f1acd8eccf35f943f973b8c3415671eaef15a154
yi0522 <5700351@gmail.com>
2022-03-20 23:03:01 +0800

    basic test

---
+++ pwd.py
@@ -0,0 +1,12 @@
+password = 'a123456'
+x = 3 #總次數
+while True:
+    pwd = input('請輸入密碼:')
+    if pwd == password:
+        print('登入成功!')
+        break
+    else:
+    	x = x - 1
+        print('密碼錯誤!還有，x，次機會!')
+        if x == 0:
+        	break
